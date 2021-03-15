4ce78ffe70604a0b190c17ce664fb6c1558aeeb3
wingedli <wingedli@tencent.com>
2021-03-15 11:34:38 +0800

    删除udp模块，修改podspec

--- Source/CocoaAsyncSocket.h
+++ Source/CocoaAsyncSocket.h
@@ -15,4 +15,3 @@ FOUNDATION_EXPORT double cocoaAsyncSocketVersionNumber;
 FOUNDATION_EXPORT const unsigned char cocoaAsyncSocketVersionString[];
 
 #import <CocoaAsyncSocket/GCDAsyncSocket.h>
-#import <CocoaAsyncSocket/GCDAsyncUdpSocket.h>
